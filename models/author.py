37326ff58611f6319ae1a67f832fb2000d7f619a
jammie-comimts <jay.mbugua.ph@gmail.com>
2024-06-11 00:25:35 +0300

    updated files

--- models/author.py
+++ models/author.py
@@ -1,7 +1,22 @@
 class Author:
     def __init__(self, id, name):
-        self.id = id
-        self.name = name
+        # Validate data types and values
+        if not isinstance(id, int):
+            raise ValueError("Author ID must be an integer")
+        if not isinstance(name, str) or len(name) == 0:
+            raise ValueError("Author name must be a non-empty string")
+
+        self._id = id  # Use private attribute (optional)
+        self._name = name
+
+    @property
+    def id(self):
+        return self._id  # Getter for ID
+
+    @property
+    def name(self):
+        return self._name  # Getter for name
 
     def __repr__(self):
         return f'<Author {self.name}>'
+
--- models/magazine.py
+++ models/magazine.py
@@ -1,8 +1,29 @@
 class Magazine:
     def __init__(self, id, name, category):
-        self.id = id
-        self.name = name
-        self.category = category
+        # Validate data types and potentially name/category length
+        if not isinstance(id, int):
+            raise ValueError("Magazine ID must be an integer")
+        if not isinstance(name, str) or len(name) == 0:
+            raise ValueError("Magazine name must be a non-empty string")
+        if not isinstance(category, str) or len(category) == 0:
+            raise ValueError("Magazine category must be a non-empty string")
+
+        self._id = id
+        self._name = name
+        self._category = category
+
+    @property
+    def id(self):
+        return self._id
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def category(self):
+        return self._category
 
     def __repr__(self):
         return f'<Magazine {self.name}>'
+
