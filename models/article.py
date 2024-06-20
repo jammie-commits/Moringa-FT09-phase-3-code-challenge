26dc0af39f7e9a51466984ba1518c9f1d5f3fd94
jammie-comimts <jay.mbugua.ph@gmail.com>
2024-06-20 16:00:08 +0300

    modified files

--- models/article.py
+++ models/article.py
@@ -1,38 +1,10 @@
 class Article:
     def __init__(self, id, title, content, author_id, magazine_id):
-        # Validate data types and potentially content length
-        if not isinstance(id, int):
-            raise ValueError("Article ID must be an integer")
-        if not isinstance(title, str) or len(title) == 0:
-            raise ValueError("Article title must be a non-empty string")
-        # Add validation for content length if needed (e.g., minimum length)
-
-        self._id = id
-        self._title = title
-        self._content = content
-        self._author_id = author_id
-        self._magazine_id = magazine_id
-
-    @property
-    def id(self):
-        return self._id
-
-    @property
-    def title(self):
-        return self._title
-
-    @property
-    def content(self):
-        return self._content
-
-    @property
-    def author_id(self):
-        return self._author_id
-
-    @property
-    def magazine_id(self):
-        return self._magazine_id
+        self.id = id
+        self.title = title
+        self.content = content
+        self.author_id = author_id
+        self.magazine_id = magazine_id
 
     def __repr__(self):
         return f'<Article {self.title}>'
-
--- models/magazine.py
+++ models/magazine.py
@@ -1,29 +1,8 @@
 class Magazine:
     def __init__(self, id, name, category):
-        # Validate data types and potentially name/category length
-        if not isinstance(id, int):
-            raise ValueError("Magazine ID must be an integer")
-        if not isinstance(name, str) or len(name) == 0:
-            raise ValueError("Magazine name must be a non-empty string")
-        if not isinstance(category, str) or len(category) == 0:
-            raise ValueError("Magazine category must be a non-empty string")
-
-        self._id = id
-        self._name = name
-        self._category = category
-
-    @property
-    def id(self):
-        return self._id
-
-    @property
-    def name(self):
-        return self._name
-
-    @property
-    def category(self):
-        return self._category
+        self.id = id
+        self.name = name
+        self.category = category
 
     def __repr__(self):
         return f'<Magazine {self.name}>'
-
